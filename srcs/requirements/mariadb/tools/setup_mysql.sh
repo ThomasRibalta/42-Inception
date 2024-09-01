d2bd3a275129057bb933ac91cadd9b10a06ceb7a
Thomas <thomasribalta@gmail.com>
2024-09-01 18:24:16 +0200

    add bonus

--- srcs/requirements/mariadb/tools/setup_mysql.sh
+++ srcs/requirements/mariadb/tools/setup_mysql.sh
@@ -2,16 +2,24 @@
 
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+sleep 5
 
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then 
+    echo "Database already exists"
+else
+    mysql -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    
+    mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+    mysql -e "FLUSH PRIVILEGES;"
+fi
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -3,15 +3,16 @@ sleep 10
 
 WORDPRESS_PATH='/var/www/wordpress'
 
-if [ ! -f "$WORDPRESS_PATH/wp-config.php" ]; then
+cd $WORDPRESS_PATH
+
+if [ ! -f "wp-config.php" ]; then
     echo "Creating wp-config.php file..."
     
     wp config create --allow-root \
                      --dbname=$MYSQL_DATABASE \
                      --dbuser=$MYSQL_USER \
                      --dbpass=$MYSQL_PASSWORD \
-                     --dbhost=mariadb:3306 \
-                     --path=$WORDPRESS_PATH
+                     --dbhost=mariadb:3306
 
     echo "wp-config.php file created successfully."
 
@@ -20,8 +21,7 @@ if [ ! -f "$WORDPRESS_PATH/wp-config.php" ]; then
                     --title="$WP_TITLE" \
                     --admin_user="$WP_ADMIN_USER" \
                     --admin_password="$WP_ADMIN_PASSWORD" \
-                    --admin_email="$WP_ADMIN_EMAIL" \
-                    --path=$WORDPRESS_PATH
+                    --admin_email="$WP_ADMIN_EMAIL"
 
     echo "WordPress installed successfully."
 
@@ -29,10 +29,18 @@ if [ ! -f "$WORDPRESS_PATH/wp-config.php" ]; then
                    $WP_USER \
                    $WP_USER_EMAIL \
                    --role=editor \
-                   --user_pass=$WP_USER_PASSWORD \
-                   --path=$WORDPRESS_PATH
+                   --user_pass=$WP_USER_PASSWORD
 
     echo "Second WordPress user created successfully."
+
+    echo "Configuring Redis..."
+    wp config set WP_REDIS_HOST redis --allow-root 
+    wp config set WP_REDIS_PORT 6379 --raw --allow-root
+    wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+    wp config set WP_REDIS_CLIENT phpredis --allow-root
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+    wp redis enable --allow-root
 else
     echo "wp-config.php already exists. Skipping configuration."
 fi
@@ -40,5 +48,4 @@ fi
 if [ ! -d "/run/php" ]; then
     mkdir -p /run/php
     echo "Created /run/php directory."
-fi
-
+fis 
